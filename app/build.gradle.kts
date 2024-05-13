d746a5ba1c3b2d74edd3a8fa05d9640e83ae1f77
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-13 22:08:03 +0530

    changes in interfaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // ROOM
+    // Use room DB
     val roomVersion = "2.6.1"
     implementation ("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
